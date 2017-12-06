d004b80be30fcca0d9d3c7a5818e16d453ffd413
sichao zheng <sichao@sichaodemacbook.home>
2017-12-06 00:00:33 +0000

    new test gaap.py

--- examples/gaap.py
+++ examples/gaap.py
@@ -6,7 +6,7 @@ from xbrl import XBRLParser, GAAP, GAAPSerializer, DEISerializer
 xbrl_parser = XBRLParser(precision=0)
 
 # Parse an incoming XBRL file
-xbrl = xbrl_parser.parse(file("../tests/sam-20130629.xml"))
+xbrl = xbrl_parser.parse(open("../tests/sam-20130629.xml"))
 
 # Parse just the GAAP data from the xbrl object
 gaap_obj = xbrl_parser.parseGAAP(xbrl,
@@ -19,22 +19,22 @@ serializer = GAAPSerializer()
 result = serializer.dump(gaap_obj)
 
 # Print out the serialized GAAP data
-print result
+print (result[0].keys())
 
 
-# Parse just the DEI data from the xbrl object
-dei_obj = xbrl_parser.parseDEI(xbrl)
-
-# Serialize the DEI data
-serializer = DEISerializer()
-result = serializer.dump(dei_obj)
-
-# Print out the serialized DEI data
-print result
-
-
-# Parse just the Custom data from the xbrl object
-custom_obj = xbrl_parser.parseCustom(xbrl)
-
-# Print out the Custom data as an array of tuples
-print custom_obj()
+## Parse just the DEI data from the xbrl object
+#dei_obj = xbrl_parser.parseDEI(xbrl)
+#
+## Serialize the DEI data
+#serializer = DEISerializer()
+#result = serializer.dump(dei_obj)
+#
+## Print out the serialized DEI data
+#print (result[0])
+#
+#
+## Parse just the Custom data from the xbrl object
+#custom_obj = xbrl_parser.parseCustom(xbrl)
+#
+## Print out the Custom data as an array of tuples
+#print (custom_obj())
---
+++ examples/gaap1.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+# encoding: utf-8
+import os
+import re
+import pandas as pd
+from xbrl import XBRLParser, GAAP, GAAPSerializer, DEISerializer
+
+Extracted_fields=['liabilities', 'temporary_equity', 'other_comprehensive_income', 'assets', 'common_shares_issued', 'other_operating_income', 'commitments_and_contingencies', 'income_loss', 'current_assets', 'operating_expenses', 'interest_and_debt_expense', 'common_shares_authorized', 'comprehensive_income_parent', 'net_income_loss', 'common_shares_outstanding', 'net_income_loss_noncontrolling', 'net_cash_flows_investing_discontinued', 'net_cash_flows_discontinued', 'nonoperating_income_loss', 'net_cash_flows_financing_continuing', 'equity_attributable_parent', 'noncurrent_liabilities', 'income_tax_expense_benefit', 'revenue', 'net_cash_flows_investing_continuing', 'cost_of_revenue', 'comprehensive_income', 'net_cash_flows_operating_discontinued', 'stockholders_equity', 'income_from_equity_investments', 'comprehensive_income_interest', 'net_income_shareholders', 'redeemable_noncontrolling_interest', 'net_cash_flows_financing', 'equity_attributable_interest', 'extraordary_items_gain_loss', 'preferred_stock_dividends', 'income_before_equity_investments', 'net_cash_flows_operating', 'equity', 'non_current_assets', 'net_income_parent', 'net_cash_flows_operating_continuing', 'gross_profit', 'costs_and_expenses', 'liabilities_and_equity', 'operating_income_loss', 'net_cash_flows_investing', 'current_liabilities']
+
+xbrl_parser = XBRLParser(precision=0)
+# Serialize the GAAP data
+serializer = GAAPSerializer()
+df = pd.DataFrame(columns=Extracted_fields)
+df.index.names = ['Company-date']
+xml_path="~/apps/repo/python-xbrl/tests"
+for root, dirs, files in os.walk("."):
+    path = root.split(os.sep)
+    print((len(path) -1) * '---', os.path.basename(root))
+    for file in files:
+        print (file)
+        if re.match(r"[a-z]{3,5}\-[0-9]{8}\.xml",file):
+            # Parse an incoming XBRL file
+            xbrl = xbrl_parser.parse(file)
+            print("Process {}".format(os.path.join(root,file)))
+            date_of_doc = re.search(r"[0-9]{8}",file).group()
+            # Parse just the GAAP data from the xbrl object
+            gaap_obj = xbrl_parser.parseGAAP(xbrl,
+                                 	     doc_date="20130629",
+                                 	     context="current",
+                                 	     ignore_errors=0)
+            
+            result = serializer.dump(gaap_obj)
+            
+            # Print out the serialized GAAP data
+            # print (result)
+            attribute = [x.encode('ascii') for x in result[0].keys()]
+            values = result[0].values()
+            df.loc[file] = values
+print (df)
+
+
+## Parse just the DEI data from the xbrl object
+#dei_obj = xbrl_parser.parseDEI(xbrl)
+#
+## Serialize the DEI data
+#serializer = DEISerializer()
+#result = serializer.dump(dei_obj)
+#
+## Print out the serialized DEI data
+#print (result)
+#
+#
+## Parse just the Custom data from the xbrl object
+#custom_obj = xbrl_parser.parseCustom(xbrl)
+#
+## Print out the Custom data as an array of tuples
+#print (custom_obj())
